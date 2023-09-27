e355152ae8a5480c2601f85aae118e9c676b593e
Mark O'Brien <mark.obrien@techniplas.com>
2023-09-26 22:05:50 -0400

    Second commit

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://$(hostname):$(port)/';
+});
+
+
+/*
 //Below print two lines of statements
-//console.log("Hello World!");
-//console.log("I'm learning JavaScript");
+console.log("Hello World!");
+console.log("I'm learning JavaScript");
+*/
 
+/*
 //Printing numbers
-//console.log(1);
-//console.log(2);
-//console.log(3);
+console.log(1);
+console.log(2);
+console.log(3);
+*/
 
+/*
 //Using a variable
-//let message = "Hello World!"
-//console.log(message)
+let message = "Hello World!"
+console.log(message)
 
+/*
 message = "Hello World!"
 //print (message)
 console.log (message)
@@ -24,3 +46,4 @@ let age = "old";
 console.log(name);
 console.log(job);
 console.log(age);
+*/
