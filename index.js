767f0ec0e63db561cfeb5d02d2bcbbd94abc2f26
Mark O'Brien <mark.obrien@techniplas.com>
2023-09-26 20:59:11 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Below print two lines of statements
+//console.log("Hello World!");
+//console.log("I'm learning JavaScript");
+
+//Printing numbers
+//console.log(1);
+//console.log(2);
+//console.log(3);
+
+//Using a variable
+//let message = "Hello World!"
+//console.log(message)
+
+message = "Hello World!"
+//print (message)
+console.log (message)
+message = "Nice weather!"
+//print (message)
+console.log(message)
+
+let name = "Mark";
+let job = "IT";
+let age = "old";
+console.log(name);
+console.log(job);
+console.log(age);
